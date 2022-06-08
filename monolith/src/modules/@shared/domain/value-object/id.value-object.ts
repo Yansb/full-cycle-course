6fa897e2b18c83f2b05c720e239ccf72ce2427fe
Yansb <yansbarreiro@gmail.com>
2022-06-08 00:14:30 -0300

    feat: create shared entities

---
+++ monolith/src/modules/@shared/domain/value-object/id.value-object.ts
@@ -0,0 +1,16 @@
+
+import { uuid} from 'uuid';
+
+import {ValueObject} from './value-object.interface';
+
+export class Id implements ValueObject {
+  private _id: string;
+
+  constructor(id?: string){
+    this._id = id || uuid();
+  }
+
+  get id(): string {
+    return this._id
+  }
+}
