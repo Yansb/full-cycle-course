93a5b286b3a074eec3457cb0ff456d1b9d86d835
yansb <yansbarreiro@gmail.com>
2023-06-30 21:52:03 -0300

    refact(event-driven): create consumer app

---
+++ event-driven/consumer/src/test/java/com/walletcore/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletcore.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
