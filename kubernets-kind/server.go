c3e9b823364e7ee4058c7db51b9c16a2b2f1f1d3
yansb <yansbarreiro@gmail.com>
2023-09-11 19:56:21 -0300

    feat(k8s): liveness probe

--- kubernets-kind/server.go
+++ kubernets-kind/server.go
@@ -5,11 +5,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+var startedAt = time.Now()
 
 func main() {
 	fmt.Println("Server started on port 8000")
 	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/", Hello)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
@@ -21,6 +26,13 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s years old.", name, age)
 }
 
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s Password: %s", user, password)
+}
+
 func ConfigMap(w http.ResponseWriter, r *http.Request) {
 	data, err := os.ReadFile("/go/myfamily/family.txt")
 
@@ -29,4 +41,16 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "My family: %s ", string(data))
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 10 || duration.Seconds() > 30 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("OK")))
+	}
 }
