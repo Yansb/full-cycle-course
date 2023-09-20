6b86592c06fe8a0bdd5a02b266a98b00d5af01ce
yansb <yansbarreiro@gmail.com>
2023-09-19 22:43:20 -0300

    feat(k8s): statefulsets and auto scaler

--- kubernets-kind/server.go
+++ kubernets-kind/server.go
@@ -46,7 +46,7 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	duration := time.Since(startedAt)
 
-	if duration.Seconds() < 10 || duration.Seconds() > 30 {
+	if duration.Seconds() < 10 {
 		w.WriteHeader(500)
 		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
 	} else {
