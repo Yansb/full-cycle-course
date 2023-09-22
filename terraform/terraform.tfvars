b85248aa8b045e604bd2d90d81e97d170661eb9d
yansb <yansbarreiro@gmail.com>
2023-09-21 22:52:15 -0300

    feat(terraform):  aws eks creation

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,7 @@
+prefix            = "yan"
+subnets_quantity  = 2
+cluster_name      = "cursofc"
+retention_days    = 90
+node_desired_size = 2
+node_max_size     = 4
+node_min_size     = 2
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "prefix" {}
+variable "subnets_quantity" {}
+variable "cluster_name" {}
+variable "retention_days" {}
+variable "node_desired_size" {}
+variable "node_max_size" {}
+variable "node_min_size" {}
